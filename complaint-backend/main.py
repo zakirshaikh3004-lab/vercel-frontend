775a168d01424b9a9d412eb2799c1e3ba39aebb0
zakirshaikh3004-lab <zakirshaikh3004@gmail.com>
2025-09-25 23:46:01 +0530

    main.py

--- complaint-backend/main.py
+++ complaint-backend/main.py
@@ -163,4 +163,5 @@ def get_departments():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
